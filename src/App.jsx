3a9187125511e20cd643674ef914451c15d736ee
aditya <asadityasingh2572000@gmail.com>
2024-06-05 15:11:46 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,8 @@ import { TermsAndCon } from "./components/TermsAndCon";
 import RequestServices from "./components/RequestServices";
 import { Contactss } from "./components/Contact";
 import GoToTop from "./components/goToTop/GoToTop";
-<<<<<<< HEAD
+
 import { ServicesParent } from "./components/Services";
-=======
-import WorkProcess from "./components/workProcess/WorkProcess";
-<<<<<<< HEAD
-=======
->>>>>>> 000816f87b5bffd0e2f57a478206e3739ffdc2d6
->>>>>>> c73b7c9bfb7f348d2d784d4e2005bf89c87aef81
->>>>>>> 5aec1aeb43fbb30c727795e5a8c5a268fbd35be6
 
 function App() {
   return (
@@ -37,15 +30,6 @@ function App() {
         <Contacts />
         <GoToTop />
         <Footer />
-<<<<<<< HEAD
-        
-=======
-        <WorkProcess />
-<<<<<<< HEAD
-=======
->>>>>>> 000816f87b5bffd0e2f57a478206e3739ffdc2d6
->>>>>>> c73b7c9bfb7f348d2d784d4e2005bf89c87aef81
->>>>>>> 5aec1aeb43fbb30c727795e5a8c5a268fbd35be6
       </div>
     </>
   );
