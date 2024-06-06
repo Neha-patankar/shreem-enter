b73c623a96c39515d9d471e105034fb22dc04309
karishma <karishmadawar123@gmail.com>
2024-06-06 16:39:13 +0530

    chnges

--- src/Components copy/ContactDetail.jsx
+++ src/Components copy/ContactDetail.jsx
@@ -1,10 +1,19 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import "./Contact.css";
 
 const ContactDetail = () => {
+  const borderRef = useRef(null);
+  const handleHover = () => {
+    console.log('Hey')
+    borderRef.current.style.border = "4px dashed #fff";
+  };
+
+  const handleRemoveHover = ()=> {
+    borderRef.current.style.border = "4px dashed #2f3369  ";
+  }
   return (
     <>
-      <div className="container-fluid " style={{ paddingTop: "150px"}}>
+      <div className="container-fluid " style={{ paddingTop: "150px" }}>
         <div className="text-center">
           <h1>Let's Talk to us.</h1>
           <p>
@@ -14,61 +23,77 @@ const ContactDetail = () => {
         </div>
         <div className="row">
           <div className="d-flex flex-wrap justify-content-center gap-5 my-5">
-            <div className="contact_box">
-            <div className="circle">
-            <i class="fa-solid fa-phone icon"></i>
-            </div>
-              
-              <div className="mt-5">
+            <div
+              className="contact_box"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleRemoveHover}
+            >
+              <div>
+                <i class="fa-solid fa-phone icon" ref={borderRef}></i>
+              </div>
+              <div className="mt-3">
                 <h5 className="info_Heading">Contact Us :</h5>
               </div>
               <div>
                 <a href="tel:8602440712" className="details">
-             +91-860-244-0712
-              </a></div>
+                  <span style={{ color: "inherit" }}>+91-860-244-0712</span>
+                </a>
+              </div>
             </div>
-            <div className="contact_box">
-            <div className="circle2"><i class="fa-solid fa-envelope icon"></i></div>
-              <div className="mt-5">
+            <div
+              className="contact_box"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleRemoveHover}
+            >
+              <div className="circle2">
+                <i class="fa-solid fa-envelope icon" ref={borderRef}></i>
+              </div>
+              <div className="mt-3">
                 <h5 className="info_Heading">Mail Us :</h5>
               </div>
-              <div><a
-                href="mailto:info@email.com"
-                className="details"
-              >
-                info@email.com
-              </a></div>
+              <div>
+                <a href="mailto:info@email.com" className="details">
+                  info@email.com
+                </a>
+              </div>
             </div>
-            <div className="contact_box">
-              <div className="circle3"></div>
-              <i
-                class="fa-solid fa-location-dot icon"
-              ></i>
-              <div className="mt-5">
+            <div
+              className="contact_box"
+              onMouseEnter={handleHover}
+              onMouseLeave={handleRemoveHover}
+            >
+              <div>
+                <i ref={borderRef} class="fa-solid fa-location-dot icon"></i>
+              </div>
+              <div className="mt-4">
                 <h5 className="info_Heading">Location :</h5>
               </div>
-              <div><a
-                href="https://maps.app.goo.gl/njbhrYWGjWzTgy469"
-                className="details"
-              >
-                28,Surya Colony,behind Sarvadharam <br /> Sector-C Kolar Road
-                Bhopal(M.P)-462042
-              </a></div>
+              <div>
+                <a
+                  href="https://maps.app.goo.gl/njbhrYWGjWzTgy469"
+                  className="details"
+                >
+                  28,Surya Colony,behind Sarvadharam Sector-C Kolar Road
+                  Bhopal(M.P) - 462042
+                </a>
+              </div>
             </div>
           </div>
 
-          <div className="w-100 d-flex justify-content-center py-5" style={{backgroundColor:'#2f3369'}}>
-          
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1833.8852139319583!2d77.41787103854367!3d23.17857751047634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c43740e860d17%3A0x924bc415766ed7a2!2sSarvadharam%20C%20Sector%2C%20Shirdipuram%2C%20Kolar%20Rd%2C%20Bhopal%2C%20Gehun%20Kheda%2C%20Madhya%20Pradesh%20462042!5e0!3m2!1sen!2sin!4v1716974635044!5m2!1sen!2sin"
-                width="1000"
-                height="400"
-                className="location-map"
-                allowfullscreen=""
-                loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
-              ></iframe>
-            </div>
+          <div
+            className="w-100 d-flex justify-content-center py-5"
+            style={{ backgroundColor: "#F5F6FA" }}
+          >
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1833.8852139319583!2d77.41787103854367!3d23.17857751047634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c43740e860d17%3A0x924bc415766ed7a2!2sSarvadharam%20C%20Sector%2C%20Shirdipuram%2C%20Kolar%20Rd%2C%20Bhopal%2C%20Gehun%20Kheda%2C%20Madhya%20Pradesh%20462042!5e0!3m2!1sen!2sin!4v1716974635044!5m2!1sen!2sin"
+              width="1000"
+              height="400"
+              className="location-map"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
       </div>
       {/* <div className="container-fluid Contact_container_bg" id="contact">
