a2892952c597bd99463038526437ee6c79cf836f
karishma <karishmadawar123@gmail.com>
2024-06-06 12:39:21 +0530

    chnges

--- src/Components copy/Faq.jsx
+++ src/Components copy/Faq.jsx
@@ -31,30 +31,7 @@ const Faq = () => {
         </div>
       </div>
     </div>
-
-    // <div className="container-fluid row" style={{backgroundColor:' #2F3369'}}>
-    //   {/* Starting of Heading */}
-    // <div className="text-center">
-    //     <h1 className="small-title mt-5">
-    //       FAQ
-    //     </h1>
-    //  <p className="big-title text-white">Got Questions? Find Your Answers Here!</p>
-    //   </div>
-    //   {/* Ending of Heading */}
-
-    //   <div className="col-lg-6 faq_img">
-    //     <img src={faq_image} alt="" className="faq_img img-fluid"/>
-    //   </div>
-
-    //   <div className="accordion_section col-lg-6 pt-3">
-
-    //     {/* Starting of Accordions */}
-    //     <div className="border m-5">
-    //       <Accordion />
-    //     </div>
-    //   </div>
-    // </div>
-  );
+      );
 };
 
 export default Faq;
