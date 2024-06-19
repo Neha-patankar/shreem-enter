541a938885ac25fc5068a7746c06e3afbadc77c1
karishma <karishmadawar123@gmail.com>
2024-06-19 11:43:11 +0530

    changes

--- src/Components copy/Faq.jsx
+++ src/Components copy/Faq.jsx
@@ -1,41 +1,40 @@
 import React, { useEffect, useState } from "react";
 import AOS from "aos";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import Accordion from "./Accordion";
 import faq_image from "./assets/Image/FAQ2.png";
 import "./Faq.css";
 
 const Faq = () => {
   useEffect(() => {
-    AOS.init({duration: "3000", })
-    }, [])
+    AOS.init({ duration: "3000", delay: 1000 });
+  }, []);
   return (
-    <div data-aos="fade-down">
-    <div className="container-fluid mt-3 mb-5 light_bg py-5 px-5">
-      <div className="row ">
-        {/* starting of Heading */}
-        <div className="text-center mt-2">
-          <h1>Got Questions? Find Your Answers Here!</h1>
-        </div>
-     
-        {/* Ending of Heading */}
-       
-        <div className="col-lg-6 d-flex justify-content-center align-items-center">
+    <div className="container-fluid light_bg py-5 px-5">
+      {/* starting of Heading */}
+      <div className="text-center my-2">
+        <h1>Got Questions? Find Your Answers Here!</h1>
+      </div>
+       {/* Ending of Heading */}
+      <div className="row">
+        
+        <div className="col-lg-6">
+          <div data-aos="fade-right">
           <div className="faq_img">
             <img src={faq_image} alt="" className="faq_img img-fluid" />
           </div>
-          </div>
-    
+        </div>
+        </div>
 
-        
         <div className="col-lg-6">
+        <div data-aos="fade-left">
           {/* Starting of Accordions */}
           <div className="m-5">
             <Accordion />
           </div>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
--- src/Components copy/OrderServiceForm.jsx
+++ src/Components copy/OrderServiceForm.jsx
@@ -1,43 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./ReqService.css";
 import Form from "./Form";
 import imgs from "./assets/Image/ContactUs.webp";
 
 const OrderServiceForm = () => {
-
+  useEffect(() => {
+    AOS.init({ duration: "500", delay: 1000 });
+  }, []);
 
   return (
-    
-    <div
-      className="container-fluid"
-      id="requestservices"
-    >
+    <div className="container-fluid" id="requestservices">
       {/* heading */}
       <div className="mt-5">
         <h1 className="text-center Order_Service_heading mb-3">
           Request top-notch service from us
         </h1>
-     
-      {/* end of heading */}
 
-      {/* starting of Request a service form */}
+        {/* end of heading */}
 
-      <div className="d-flex justify-content-center">
-        <div className=" row">
-          {/* illustration */}
-          <div className="col-lg-6">
-            <img src={imgs} alt="" className="OS_image img-fluid " />
+        {/* starting of Request a service form */}
+
+        <div className="d-flex justify-content-center">
+          <div className=" row">
+            {/* illustration */}
+            <div className="col-lg-6">
+              <div data-aos=" fade-up">
+                <img src={imgs} alt="" className="OS_image img-fluid " />
+              </div>
+            </div>
+            {/* end of illustration */}
+
+            {/* Starting of form */}
+            <div className="col-lg-6">
+            <Form />
+            </div>
           </div>
-          
-          {/* end of illustration */}
-
-          {/* Starting of form */}
-          <Form/>
-        </div>
         </div>
       </div>
     </div>
-    
   );
 };
 
